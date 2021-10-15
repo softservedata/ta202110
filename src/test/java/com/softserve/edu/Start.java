2051cf371bf13860b149a7b790259f8c648df6ec
Softservedata <garasym@gmail.com>
2021-10-15 11:16:32 +0300

    Template maven project

---
+++ src/test/java/com/softserve/edu/Start.java
@@ -0,0 +1,12 @@
+package com.softserve.edu;
+
+import org.testng.annotations.Test;
+
+public class Start {
+
+    @Test
+    public void testApp() {
+        // From Maven
+        System.out.println("Proba testApp()");
+    }
+}
