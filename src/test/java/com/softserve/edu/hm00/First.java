34b66481ba7910b41a9926bd8bf25d59d21f442a
Softservedata <garasym@gmail.com>
2021-10-20 09:45:50 +0300

    crete hw00

---
+++ src/test/java/com/softserve/edu/hm00/First.java
@@ -0,0 +1,11 @@
+package com.softserve.edu.hm00;
+
+import org.testng.annotations.Test;
+
+public class First {
+
+    @Test
+    void testAdd1() {
+        System.out.println("code ...");
+    }
+}
