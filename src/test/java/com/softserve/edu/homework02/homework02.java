fcaf52fc123217f20062c52f6b608575697fbfb0
APietina <a.petina.chrome@gmail.com>
2021-10-23 00:15:32 +0300

    message 1

---
+++ src/test/java/com/softserve/edu/homework02/homework02.java
@@ -0,0 +1,12 @@
+package com.softserve.edu.homework02;
+
+import org.testng.annotations.Test;
+
+public class homework02 {
+
+    @Test
+    void testAdd1() {
+        System.out.println("test1");
+    }
+
+}
