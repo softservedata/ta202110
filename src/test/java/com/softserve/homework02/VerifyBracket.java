eb5837dbb2b236ec13facf1c2a31d2480d9b516e
MAIN_PC <shevnastasya@gmail.com>
2021-10-29 01:28:06 +0300

    homework3 partly finished

--- src/test/java/com/softserve/homework02/VerifyBracket.java
+++ src/test/java/com/softserve/homework02/VerifyBracket.java
@@ -2,46 +2,46 @@ package com.softserve.homework02;
 
 import org.junit.Test;
 
-public class VerifyBracket {
-
-@Test
-
-// Code doesn't work. I stuck here, don't know how to fix it
-
-    public class verifyBrackets{
-    
-    private const String bracket = ")";
-    private const String backbracket= "(";
-
-        public <var> boolean verifyBrackets(String text) {
-            ArrayList<int> bracketIndexes = new ArrayList<int>();
-            for (var i = 0; i < text.Length; i++)
-            { if(text[i]== bracket)
-            { bracketIndexes.Add(text[i]);
-            }
-            }
-
-            return false;
-        }
-
-
-
-   
-
-//      boolean () = true;
-//    System.out.println( () );
-//        boolean (()) = true;
-//    System.out.println( "Success" + (()));
-//        boolean (())() = true;
-//    System.out.println( "Success" + (())());
+//public class VerifyBracket {
 //
-//        boolean (() = false;
-//    System.out.println( "Failed" + (());
-//        boolean )( = false;
-//    System.out.println( "Failed" + )();
-//        boolean ()) = false;
-//    System.out.println( "Failed" + ()));
-
-    }
-
-}
+//@Test
+//
+//// Code doesn't work. I stuck here, don't know how to fix it
+//
+////    public class verifyBrackets{
+////
+////    private const String bracket = ")";
+////    private const String backbracket= "(";
+////
+////        public <var> boolean verifyBrackets(String text) {
+////            ArrayList<int> bracketIndexes = new ArrayList<int>();
+////            for (var i = 0; i < text.Length; i++)
+////            { if(text[i]== bracket)
+////            { bracketIndexes.Add(text[i]);
+////            }
+////            }
+////
+////            return false;
+////        }
+//
+//
+//
+//
+//
+////      boolean () = true;
+////    System.out.println( () );
+////        boolean (()) = true;
+////    System.out.println( "Success" + (()));
+////        boolean (())() = true;
+////    System.out.println( "Success" + (())());
+////
+////        boolean (() = false;
+////    System.out.println( "Failed" + (());
+////        boolean )( = false;
+////    System.out.println( "Failed" + )();
+////        boolean ()) = false;
+////    System.out.println( "Failed" + ()));
+//
+//    }
+//
+//}
