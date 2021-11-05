6d10dbd3e2e3ea77c5644615c2a6d4ea852d66de
Softservedata <garasym@gmail.com>
2021-11-05 11:51:51 +0200

    Page Object. TopPart

---
+++ src/test/java/com/softserve/edu/opencart/pages/AccountSidebarPart.java
@@ -0,0 +1,22 @@
+package com.softserve.edu.opencart.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class AccountSidebarPart extends TopPart {
+
+    public AccountSidebarPart(WebDriver driver) {
+        super(driver);
+        initElements();
+    }
+
+    private void initElements() {
+        // init elements
+    }
+
+    // Page Object
+
+    // Functional
+
+    // Business Logic
+
+}
--- src/test/java/com/softserve/edu05/ScrolleTest.java
+++ src/test/java/com/softserve/edu05/ScrolleTest.java
@@ -12,6 +12,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.ElementClickInterceptedException;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -19,6 +20,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
@@ -30,6 +34,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class ScrolleTest {
     private final String TIME_TEMPLATE = "yyyy-MM-dd_HH-mm-ss-S";
     private WebDriver driver;
+    int count = 0;
 
     private void takeScreenShot(String testname) throws IOException {
         String currentTime = new SimpleDateFormat(TIME_TEMPLATE).format(new Date());
@@ -87,25 +92,63 @@ public class ScrolleTest {
         //
         WebElement linkProject = driver.findElement(By.cssSelector("a[href='https://www.perfecto.io/']"));
         //
-        // /*
-        // Scrolling by Action class
-        Actions action = new Actions(driver);
-        action.moveToElement(linkProject).perform();
-        Thread.sleep(2000); // For Presentation Only
-        // */
-        /*-
+        /* // Scrolling by Action class
+         * Actions action = new Actions(driver);
+         * action.moveToElement(linkProject).perform();
+         * Thread.sleep(2000); // For Presentation Only */
+        // /*-
         // Scrolling by JavaScript injection
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", linkProject);
         Thread.sleep(2000); // For Presentation Only
-        */
+        // */
         //
         takeScreenShot("checkScroll");
         takePageSource("checkScroll");
+        //
         WebElement browserstack = driver.findElement(By.cssSelector("a[href*='www.browserstack.com']"));
         System.out.println("browserstack.isDisplayed() = " + browserstack.isDisplayed());
         System.out.println("browserstack.isEnabled() = " + browserstack.isEnabled());
         System.out.println("browserstack.isSelected() = " + browserstack.isSelected());
-        browserstack.click(); // Error if Scrolling by JavaScript
+        //
+        driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+        /*-
+        //        WebElement browserstack2 = (new WebDriverWait(driver, 10))
+        //            .until(ExpectedConditions.elementToBeClickable(By.cssSelector("a[href*='www.browserstack.com']")));
+        WebElement browserstack2 = (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a[href*='www.browserstack.com']")));
+         browserstack2.click();
+        */
+        //
+        //int count = 0;
+        (new WebDriverWait(driver, 10)).until(new 
+                ExpectedCondition<Boolean>() {
+                    public Boolean apply(WebDriver driver) {
+                        count++;
+                        System.out.println("count = " + count);
+                        if (count > 4) {
+                            WebElement button2 = driver.findElement(By.cssSelector("button.btn.btn-dark.btn-lg.btn-block"));
+                            ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", button2);
+                            //WebElement element2 = driver.findElement(By.cssSelector("a[href*='www.browserstack.com']"));
+                            //Actions action = new Actions(driver);
+                            //action.moveToElement(element2).perform();
+                        }
+                        try {
+                            browserstack.click();
+                        } catch (ElementClickInterceptedException e) {
+                            return false;
+                        }
+                        return true;
+            }    }    );
+        /*-
+        WebElement browserstack2 = (new WebDriverWait(driver, 10))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("a[href*='www.browserstack.com']")));
+        try {
+            browserstack2.click(); // Error
+        } catch (ElementClickInterceptedException e) {
+        }
+        */
+        //
+        //        browserstack.click(); // Error if Scrolling by JavaScript
         Thread.sleep(4000); // For Presentation Only
     }
 }
