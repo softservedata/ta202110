64a3708c4b84848f4c3540305b7ace169a2c3901
MAIN_PC <shevnastasya@gmail.com>
2021-11-08 21:54:29 +0200

    homework 3  full + some edits

--- src/test/java/com/softserve/homework03/FirstClass.java
+++ src/test/java/com/softserve/homework03/FirstClass.java
@@ -1,7 +1,8 @@
-//package com.softserve.homework03;
+package com.softserve.homework03;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -9,35 +10,36 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
-//
-//public class FirstClass {
-//
-//    @Test
-//
-//    public void searchWebElement(){
-//        WebDriverManager.chromedriver().setup();
-//        WebDriver driver = new ChromeDriver();
-//        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//        driver.manage().window().maximize();
-//
-//        driver.get("http://taqc-opencart.epizy.com/");
-//
-//        driver.findElement(By.cssSelector("button.btn.btn-link.dropdown-toggle")).click();
-//        driver.findElement(By.cssSelector("button[name='EUR']")).click();
-//        //
-//        driver.findElement(By.name("search")).sendKeys("iPhone");
-//        driver.findElement(By.cssSelector("#search button")).click();
-//
-//        WebElement price = driver
-//                .findElement(By.xpath("//a[contains(text(),'iPhone XR')]/../.."));
-//        Assert.assertTrue(price.getText().contains("707.71€"));
-//        System.out.println("***contains: " + price.getText());
-//
-//        driver.close();
-//
+
+public class FirstClass {
+
+    @Ignore
+ /*   @Test*/
+
+   public void searchWebElement(){
+       WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+
+        driver.get("http://taqc-opencart.epizy.com/");
+
+        driver.findElement(By.cssSelector("button.btn.btn-link.dropdown-toggle")).click();
+        driver.findElement(By.cssSelector("button[name='EUR']")).click();
+        //
+        driver.findElement(By.name("search")).sendKeys("iPhone");
+        driver.findElement(By.cssSelector("#search button")).click();
+
+        WebElement price = driver
+                .findElement(By.xpath("//a[contains(text(),'iPhone XR')]/../.."));
+        Assert.assertTrue(price.getText().contains("707.71€"));
+       System.out.println("***contains: " + price.getText());
+
+        driver.close();
 
 
 
 
-//    }
-//}
+
+    }
+}
---
+++ src/test/java/com/softserve/homework07/FifthImExplicitWait.java
@@ -0,0 +1,57 @@
+package com.softserve.homework07;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+public class FifthImExplicitWait {
+
+    @Test
+
+    public void ImplicitExplicitWaitCheck () {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver;
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+
+        driver.get("http://taqc-opencart.epizy.com/");
+
+
+        driver.findElement(By.xpath("//input[@name='search']")).sendKeys("MacBook");
+        driver.findElement(By.xpath("//*[@id='search']/span/button")).click();
+
+        driver.findElement(By.xpath("//a[text()='MacBook']/../..//p[contains(text(), '602')]")).click();
+        driver.findElement(By.xpath("//*[@id='content']/../..//button[1]/span")).click();
+
+
+        //check an operation success
+        String content = driver.findElement(By.xpath("//i[@class= 'fa fa-check-circle']/following-sibling::a")).getAttribute("text");//
+        System.out.println("content text1 (macbook) = " + content);
+
+        driver.findElement(By.xpath("//*[@id='search']/input")).clear();
+        driver.findElement(By.xpath("//*[@id='search']/input")).sendKeys("iPhone 3");
+        driver.findElement(By.xpath("//*[@id='search']/span/button")).click();
+        driver.findElement(By.xpath("//*[@id='content']/..//div[2]/button[2]/i")).click();
+
+
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[2]/div[1]")));
+
+        //check an operation success
+        String content1 = driver.findElement(By.xpath("//i[@class= 'fa fa-check-circle']/following-sibling::*[1]")).getAttribute("text");
+        System.out.println("content text2(iphone3) = " + content1);
+
+        driver.close();
+
+
+    }
+}
