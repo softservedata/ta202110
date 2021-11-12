b286a8267db0f20dedc8653146f3537e8726fcba
Yaroslav <garasym@gmail.com>
2021-11-12 12:00:22 +0200

    Repository. Singleton. Example

--- src/test/java/com/softserve/edu/opencart/test/LoginTest.java
+++ src/test/java/com/softserve/edu/opencart/test/LoginTest.java
@@ -4,11 +4,14 @@ import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import com.softserve.edu.opencart.data.IUser;
+import com.softserve.edu.opencart.data.UserRepository;
 import com.softserve.edu.opencart.pages.EditAccountPage;
 import com.softserve.edu.opencart.pages.HomePage;
 
 public class LoginTest extends TestRunner {
 
+    /*-
     @DataProvider
     public Object[][] dataSuccessful() {
         return new Object[][] {
@@ -16,19 +19,30 @@ public class LoginTest extends TestRunner {
             { "hahaha@gmail.com", "qwerty", "hahaha" },
         };
     }
+    */
 
+    @DataProvider
+    public Object[][] dataSuccessful() {
+        return new Object[][] {
+            //{ UserRepository.get().getHahaha() },
+            { UserRepository.get().getAwdrt() },
+        };
+    }
+    
     @Test(dataProvider = "dataSuccessful")
-    public void checkSuccessful(String email, String password, String firstName) {
+    public void checkSuccessful(IUser user) {
         //
         // Steps
         EditAccountPage editAccountPage = loadApplication()
                 .gotoLoginPage()
-                .successfulLogin(email, password)
+                //.successfulLogin(email, password)
+                .successfulLogin(user)
                 .gotoEditAccountRight();
         presentationSleep();
         //
         // Check
-        Assert.assertEquals(editAccountPage.getFirstNameFieldText(), firstName);
+        //Assert.assertEquals(editAccountPage.getFirstNameFieldText(), firstName);
+        Assert.assertEquals(editAccountPage.getFirstNameFieldText(), user.getFirstname());
         //
         // Return to Previous State
         HomePage homePage = editAccountPage
