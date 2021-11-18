7a4013313c8e16dc5c04b8cdd20d30a41ee935c2
Tetiana <femidat82@gmail.com>
2021-11-18 21:18:00 +0200

    DropdownComponent

---
+++ src/test/java/com/softserve/homework08/DropDownComponent.java
@@ -0,0 +1,35 @@
+package com.softserve.homework08;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import com.softserve.edu.opencart.data.Currencies;
+import com.softserve.edu.opencart.pages.HomePage;
+
+public class DropDownComponent {
+
+    @DataProvider(parallel = true)
+    public Object[][] searchCurrencies() {
+        return new Object[][] {
+                { Currencies.EURO },
+                //{ Currencies.POUND_STERLING },
+        };
+    }
+
+    @Test(dataProvider = "searchCurrencies")
+    public void checkFirst(Currencies currency) {
+        //
+        // Steps
+        HomePage homePage = loadApplication()
+                //.chooseCurrency("US Dollar");
+                .chooseCurrency(currency);
+        //.scrollToProduct(searchProduct.getProduct());
+        presentationSleep();
+        //
+        // Check
+        Assert.assertTrue(homePage.getCurrencyText().contains("€"));
+        //
+        presentationSleep(4);
+    }
+}
