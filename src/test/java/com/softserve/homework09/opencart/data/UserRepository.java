0ca1df37cb0a478df79e077c6990069247f6f51f
YuriiTA <hiphap@ukr.net>
2021-11-28 13:45:25 +0200

    homework09

---
+++ src/test/java/com/softserve/homework09/opencart/data/UserRepository.java
@@ -0,0 +1,89 @@
+package com.softserve.homework09.opencart.data;
+
+public final class UserRepository {
+    private static volatile UserRepository instance = null;
+
+    private UserRepository() {
+    }
+
+    public static UserRepository get() {
+        if (instance == null) {
+            synchronized (UserRepository.class) {
+                if (instance == null) {
+                    instance = new UserRepository();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public IUser getDefault() {
+        return getHahaha();
+    }
+    
+    public IUser getHahaha() {
+        return User.get()
+            .setFirstname("hahaha")
+            .setLastname("hahaha")
+            .setEmail("hahaha@gmail.com")
+            .setTelephone("hahaha")
+            .setAddress1("address1")
+            .setCity("city_5")
+            .setPostcode("postcode")
+            .setCountry("country")
+            .setRegion("region")
+            .setPassword("qwerty")
+            .setSubscribe(true)
+            .setFax("fax")
+            .setCompany("company")
+            .setAddress2("address2")
+            .build();
+    }
+    
+    public IUser getAwdrt() {
+        return User.get()
+                .setFirstname("my")
+                .setLastname("hahaha")
+                .setEmail("xdknxusqvjeovowpfk@awdrt.com")
+                .setTelephone("hahaha")
+                .setAddress1("address1")
+                .setCity("city_5")
+                .setPostcode("postcode")
+                .setCountry("country")
+                .setRegion("region")
+                .setPassword("awdrt123")
+                .setSubscribe(true)
+                .setFax("fax")
+                .setCompany("company")
+                .setAddress2("address2")
+                .build();
+    }
+    
+    /*-
+    public IUser getNewUser() {
+        
+    }
+    
+    public IUser getInvalidUser() {
+        
+    }
+    */
+    
+    /*-
+    public List<IUser> fromCsv(String filename) {
+        return User.getByLists(new CSVReader(filename).getAllCells());
+    }
+
+    public List<IUser> fromCsv() {
+        return fromCsv("users.csv");
+    }
+
+    public List<IUser> fromExcel(String filename) {
+        return User.getByLists(new ExcelReader(filename).getAllCells());
+    }
+
+    public List<IUser> fromExcel() {
+        return fromExcel("users.xlsx");
+    }
+    */
+}
