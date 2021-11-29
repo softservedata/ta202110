d48aab95455d1858d4328ad1724388b488c6b456
APietina <a.petina.chrome@gmail.com>
2021-11-29 02:01:30 +0200

    Homework 9 - ChangeCurrencyToEURTest DataProvider

---
+++ src/test/java/com/softserve/homework09/ChangeCurrencyToEURTest.java
@@ -0,0 +1,28 @@
+package com.softserve.homework09;
+
+import com.softserve.edu.opencart.data.Currencies;
+import com.softserve.edu.opencart.pages.HomePage;
+import com.softserve.edu.opencart.test.TestRunner;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class ChangeCurrencyToEURTest extends TestRunner {
+
+    @DataProvider
+    public Object[][] dataCurrencies() {
+        return new Object[][] {
+                { Currencies.EURO },
+        };
+    }
+
+    @Test(dataProvider = "dataCurrencies")
+    public void changeCurrency(Currencies currency) {
+
+        HomePage homePage = loadApplication()
+                .chooseCurrency(currency);
+
+        Assert.assertTrue(homePage.getCurrencyText().contains("€"));
+    }
+
+}
